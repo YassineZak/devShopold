2f0d04e9a0bbb4a18906f04cf7d460dad0aa935c
yassineZak <yassine.zakari@hotmail.fr>
2018-05-15 22:37:09 +0200

    ajout de l'ensemble des templates des pages

--- src/YZ/EcommerceBundle/Controller/EcommerceController.php
+++ src/YZ/EcommerceBundle/Controller/EcommerceController.php
@@ -3,6 +3,8 @@
 namespace YZ\EcommerceBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 class EcommerceController extends Controller
 {
@@ -14,5 +16,13 @@ class EcommerceController extends Controller
     public function shopAction()
     {
       return $this->render('YZEcommerceBundle:Ecommerce:shop.html.twig');
+    }
+    public function productAction()
+    {
+      return $this->render('YZEcommerceBundle:Ecommerce:product.html.twig');
+    }
+    public function cartAction()
+    {
+      return $this->render('YZEcommerceBundle:Ecommerce:cart.html.twig');
     }
 }
