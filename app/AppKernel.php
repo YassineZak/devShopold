cac0ff89b576ce7abed5208d6696df1027b225c1
yassineZak <yassine.zakari@hotmail.fr>
2018-05-16 22:55:30 +0200

    config de sonata user bundle et fos userbundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -26,6 +26,7 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new Sonata\UserBundle\SonataUserBundle(),
             new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
+            new Application\Sonata\UserBundle\ApplicationSonataUserBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
---
+++ src/YZ/UserBundle/YZUserBundle.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace YZ\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class YZUserBundle extends Bundle
+{
+  public function getParent()
+
+ {
+
+   return 'FOSUserBundle';
+
+ }
+}
