802a16b7c028be0c1f0fc6f87631c923dd3ade26
yassineZak <yassine.zakari@hotmail.fr>
2018-05-23 20:31:26 +0200

    integration de easyadminbundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new YZ\UserBundle\YZUserBundle(),
             new \Ivory\CKEditorBundle\IvoryCKEditorBundle(),
+            new EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
