de45f94d637416c03799d5f38e287b4cb4a8ec23
yassineZak <yassine.zakari@hotmail.fr>
2018-05-28 22:44:01 +0200

    ajout de produit dans la bdd

--- src/YZ/EcommerceBundle/Controller/EcommerceController.php
+++ src/YZ/EcommerceBundle/Controller/EcommerceController.php
@@ -5,6 +5,7 @@ namespace YZ\EcommerceBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class EcommerceController extends Controller
 {
