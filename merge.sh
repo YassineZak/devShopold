328d02a2ae0e32df23bf9b49cb42c0eef549dd8d
zakari yassine <yassine.zakari@hotmail.fr>
2018-08-07 18:00:26 +0200

    modification du fichier de merge.sh

--- merge.sh
+++ merge.sh
@@ -29,6 +29,7 @@ if [ "$CURRENT_BRANCH" = "$FROM_BRANCH" ] ; then
     #echo "Checking out $TO_BRANCH..." && \
 
     # Checkout the latest stable
+    git remote add $PUSH_URL
     git fetch --all && \
     git checkout $TO_BRANCH && \
     echo $GITHUB_SECRET_TOKEN && \
