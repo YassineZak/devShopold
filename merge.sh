7942afb5a146bfa3d3c3288f589ee05d3d6d91d6
zakari yassine <yassine.zakari@hotmail.fr>
2018-08-07 18:13:40 +0200

    modification du fichier merge.sh

--- merge.sh
+++ merge.sh
@@ -41,7 +41,6 @@ if [ "$CURRENT_BRANCH" = "$FROM_BRANCH" ] ; then
 
     # Push changes back to remote vcs
     echo "Pushing changes..." && \
-    git push $PUSH_URL && \
     echo "Merge complete!" || \
     echo "Error Occurred. Merge failed"
 else
